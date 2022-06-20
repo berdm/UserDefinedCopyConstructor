bf9f5eca6cb5774265b2fbf78c26a21837df07e8
Buket Erdem <46616338+berdm@users.noreply.github.com>
2022-06-20 21:28:39 +0300

    Add files via upload

---
+++ UserDefinedCopyConstructorExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserDefinedCopyConstructorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
